b31ebf634a858ccc5034a87004ee396e1f96ad36
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-04-02 20:21:34 +0200

    task 3 bug (automated command)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""doc"""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views 
 from api.v1.views import statues
+"""doc"""
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
