5e905b065fdd991a148c4755cdd496ec074f0dc7
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-04-02 21:15:05 +0200

    4. Some stats? (automated command)

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,22 @@
 """doc"""
 from api.v1.views import app_views 
 from flask import jsonify
-
+from models import storage
+from models import city, place,amenity,review,state,user
 @app_views.route("/status")
 def statues():
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats():
+    """return count of class from the database"""
+    cls_count_dict={
+  "amenities": storage.count(amenity) ,
+  "cities": storage.count(city) ,
+  "places": storage.count(place)  ,
+  "reviews": storage.count(review) ,
+  "states":  storage.count(state) ,
+  "users":  storage.count(user),
+  
+}
+    return jsonify(cls_count_dict)
